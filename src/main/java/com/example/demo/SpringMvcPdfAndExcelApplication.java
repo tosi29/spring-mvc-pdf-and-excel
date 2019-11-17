a93f1985fbce7c253fb4fd6b31eb2830f6aef5b3
tosi29 <tosi16@gmail.com>
2019-11-18 00:38:56 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/SpringMvcPdfAndExcelApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcPdfAndExcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcPdfAndExcelApplication.class, args);
+	}
+
+}
