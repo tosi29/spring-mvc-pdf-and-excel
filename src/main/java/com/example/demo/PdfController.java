bcc0cd62fc74218a53fe0a9e66cc00f7364ec127
tosi29 <tosi16@gmail.com>
2019-11-18 01:36:23 +0900

    Remove unnecessary import

--- src/main/java/com/example/demo/PdfController.java
+++ src/main/java/com/example/demo/PdfController.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PdfController {
